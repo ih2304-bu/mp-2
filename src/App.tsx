0f870a922c46f0d69e8f05457ddb23d6060873c8
Ibrahim Hussain <ih2304@bu.edu>
2025-09-28 00:55:26 -0400

    initial push

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import PoetryDB from "./components/poetrydb.tsx";
+import styled from "styled-components";
+import {useEffect, useState} from "react";
+import type {Poem} from "./interfaces/Poetry.ts";
+
+const ParentDiv=styled.div`
+    width: 80vw;
+    margin: auto;
+    border: 5px darkblue solid;
+`;
+
+export default function App(){
+
+    // useState Hook to store Data.
+    const [data, setData] = useState<Poem[]>([]);
+
+    // useEffect Hook for error handling and re-rendering.
+    useEffect(() => {
+        async function fetchData(): Promise<void> {
+            // Fetch random poems from PoetryDB API
+            const rawData = await fetch("https://poetrydb.org/random/6/author,title,lines,linecount");
+            const poems: Poem[] = await rawData.json();
+            setData(poems);
+        }
+        fetchData()
+            .then(() => console.log("Poetry data fetched successfully"))
+            .catch((e: Error) => console.log("There was an error: " + e));
+    }, [data.length]);
+
+    return(
+        <ParentDiv>
+            <PoetryDB data={data}/>
+        </ParentDiv>
+    )
+}
---
+++ src/components/poetrydb.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import type {Poem} from "../interfaces/Poetry.ts";
+
+const AllPoemsDiv=styled.div`
+    display: flex;
+    flex-flow: row wrap;    
+    justify-content: space-evenly;
+    background-color: lightblue;
+`;
+
+const SinglePoemDiv=styled.div`
+    display: flex;
+    flex-direction: column;   
+    justify-content: center;
+    max-width: 30%;
+    padding: 2%;
+    margin: 1%;
+    background-color: white;
+    color: black;
+    border: 3px darkblue solid;
+    font: italic small-caps bold calc(2px + 1vw) Georgia, serif;
+    text-align: center;
+`;
+
+export default function PoetryDB(props : { data:Poem[] } ){
+    return (
+        <AllPoemsDiv >
+            {
+                props.data.map((poem: Poem, index: number) =>
+                    <SinglePoemDiv key={`${poem.title}-${index}`}>
+                        <h1>{poem.title}</h1>
+                        <p>by {poem.author}</p>
+                        <div>{poem.lines.join('\n')}</div>
+                        <p>{poem.linecount} lines</p>
+                    </SinglePoemDiv>
+                )
+            }
+        </AllPoemsDiv>
+    );
+}
